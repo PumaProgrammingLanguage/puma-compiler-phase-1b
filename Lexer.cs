ae50e16f951fafa2f400d2d03634aca2fcfb03cc
Anthony <d.a.burchfield@att.net>
2025-08-25 20:39:13 -0500

    Added starting code for the Lexer.

---
+++ Lexer.cs
@@ -0,0 +1,23 @@
+// Lexer.cs
+namespace PumaCompiler
+{
+    public class Lexer
+    {
+        // TODO: Implement tokenization logic for Puma language
+        public Lexer(string source)
+        {
+            // Initialization code
+        }
+
+        public IEnumerable<Token> Tokenize()
+        {
+            // Tokenization logic
+            yield break;
+        }
+    }
+
+    public class Token
+    {
+        // Token properties (type, value, position, etc.)
+    }
+}
