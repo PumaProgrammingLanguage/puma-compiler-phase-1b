5b2a03ee4a124587d439785584c65add94be9723
Anthony <d.a.burchfield@att.net>
2025-08-25 20:41:53 -0500

    Added starting code for the code generator.

---
+++ CodeGen.cs
@@ -0,0 +1,18 @@
+// CodeGen.cs
+namespace PumaCompiler
+{
+    public class CodeGen
+    {
+        // TODO: Implement code generation logic for Puma language
+        public CodeGen(AstNode ast)
+        {
+            // Initialization code
+        }
+
+        public string Generate()
+        {
+            // Code generation logic
+            return string.Empty;
+        }
+    }
+}
