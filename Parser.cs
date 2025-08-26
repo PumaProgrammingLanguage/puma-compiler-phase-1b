9632dcc937412239b5d0c5300fe2a66e4ecd0f3f
Anthony <d.a.burchfield@att.net>
2025-08-25 20:41:29 -0500

    Added starting code for the Parser.

---
+++ Parser.cs
@@ -0,0 +1,23 @@
+// Parser.cs
+namespace PumaCompiler
+{
+    public class Parser
+    {
+        // TODO: Implement parsing logic for Puma language
+        public Parser(IEnumerable<Token> tokens)
+        {
+            // Initialization code
+        }
+
+        public AstNode Parse()
+        {
+            // Parsing logic
+            return null;
+        }
+    }
+
+    public class AstNode
+    {
+        // AST node properties and methods
+    }
+}
